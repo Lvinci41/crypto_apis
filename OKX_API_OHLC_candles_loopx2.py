9142d22dd68b97687a222364c60b3cc28a2d7f5d
Lvinci41 <lvinci44@gmail.com>
2025-02-27 23:20:58 +0800

    Update OKX_API_OHLC_candles_loopx2.py
    updated as standalone script

--- OKX_API_OHLC_candles_loopx2.py
+++ OKX_API_OHLC_candles_loopx2.py
@@ -49,27 +49,33 @@ def fetch_okx_ohlc_data(api_key, secret_key, passphrase, instrument_id, bar, sta
     
     return df    
 
-current_time = datetime.now(timezone.utc)
-start_time_days = 1
-end_time_days = 2700
-bar_size = "1H"
-ticker_list = ["BTC-USDT", "ETH-USDT", "LTC-USDT", "XRP-USDT", "DOGE-USDT","BTC-USDC", "ETH-USDC", "LTC-USDC", "XRP-USDC", "DOGE-USDC"]
 
-columns = ["timestamp", "open", "high", "low", "close", "volume", "volume_ccy", "volCcyQuote", "confirm"]
-start_time = int((current_time - timedelta(days=start_time_days)).timestamp() * 1000) 
-end_time = int((current_time - timedelta(days=end_time_days)).timestamp() * 1000) 
+if __name__ == '__main__':
+    try:
+        current_time = datetime.now(timezone.utc)
+        start_time_days = 1
+        end_time_days = 2700
+        bar_size = "1H"
+        ticker_list = ["BTC-USDT", "ETH-USDT", "LTC-USDT", "XRP-USDT", "DOGE-USDT","BTC-USDC", "ETH-USDC", "LTC-USDC", "XRP-USDC", "DOGE-USDC"]
+        
+        columns = ["timestamp", "open", "high", "low", "close", "volume", "volume_ccy", "volCcyQuote", "confirm"]
+        start_time = int((current_time - timedelta(days=start_time_days)).timestamp() * 1000) 
+        end_time = int((current_time - timedelta(days=end_time_days)).timestamp() * 1000) 
+        
+        for ticker in ticker_list:
+            start_time_var = start_time 
+            filename = str(dt.datetime.fromtimestamp(start_time/1000.0)).replace(":",".")[:-6].replace(":",".").replace("-","") + "-" + str(dt.datetime.fromtimestamp(end_time/1000.0)).replace(":",".")[:-6].replace(":",".").replace("-","")+"_"+ticker+"_"+bar_size+"_ohlc.csv"    
+            result = pd.DataFrame(columns=columns)
+            result.set_index("timestamp", inplace=True)    
+            while start_time_var > end_time:
+                result = result._append( fetch_okx_ohlc_data(APIKEY, APISECRET, PASS, ticker, bar_size, start_time_var, end_time) )
+                start_time_var -= 345600000 #ms in 4 days, max returned bars of 1h
+                time.sleep( random.randint(10,35)/100 ) #rate limit is 20 calls in 2 seconds
+        
+            result.drop_duplicates(inplace=True)
+            result.sort_index(inplace=True)
+            result.to_csv(filename)
+            print(result.head(), result.tail())
 
-for ticker in ticker_list:
-    start_time_var = start_time 
-    filename = str(dt.datetime.fromtimestamp(start_time/1000.0)).replace(":",".")[:-6].replace(":",".").replace("-","") + "-" + str(dt.datetime.fromtimestamp(end_time/1000.0)).replace(":",".")[:-6].replace(":",".").replace("-","")+"_"+ticker+"_"+bar_size+"_ohlc.csv"    
-    result = pd.DataFrame(columns=columns)
-    result.set_index("timestamp", inplace=True)    
-    while start_time_var > end_time:
-        result = result._append( fetch_okx_ohlc_data(APIKEY, APISECRET, PASS, ticker, bar_size, start_time_var, end_time) )
-        start_time_var -= 345600000 #ms in 4 days, max returned bars of 1h
-        time.sleep( random.randint(10,35)/100 ) #rate limit is 20 calls in 2 seconds
-
-    result.drop_duplicates(inplace=True)
-    result.sort_index(inplace=True)
-    result.to_csv(filename)
-    print(result.head(), result.tail())
+    except Exception as e:
+        print(f"Error: {e}")
